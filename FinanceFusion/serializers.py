dd99bc37b2c5bdb032d6d3446bae1e3577515d2b
Sergio Avila <sergioavila2720@gmail.com>
2024-04-16 20:37:21 -0700

    rest of CRUD with patch as well

--- FinanceFusion/serializers.py
+++ FinanceFusion/serializers.py
@@ -12,7 +12,23 @@ class CategorySearialier(serializers.ModelSerializer):
 
 class ExpenseSearialier(serializers.ModelSerializer):
     category = CategorySearialier()
-    user = User
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True)
+    user = UserSerializer()
     class Meta:
         model = Expense
-        fields = '__all__'
+        fields = ['id', 'name' ,'description', 'amount', 'is_shared', 'user', 'category', 'category_id']
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user
+        return Expense.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.category = validated_data.get('category', instance.category)
+        instance.save()
+        return instance
--- FinanceFusion/views.py
+++ FinanceFusion/views.py
@@ -29,6 +29,49 @@ class ExpenseCRUDAPIView(APIView):
             serializer = ExpenseSearialier(expenses, many=True)
             return Response(serializer.data)
 
+    def post(self, request):
+        serializer = ExpenseSearialier(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            expense = Expense.objects.get(pk=pk, user=request.user)
+        except Expense.DoesNotExist:
+            return Response({"message": "Expense not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ExpenseSearialier(expense, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        try:
+            expense = Expense.objects.get(pk=pk, user=request.user)
+        except Expense.DoesNotExist:
+            return Response({"message": "Expense not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ExpenseSearialier(expense, data=request.data, partial=True, context={'request': request})  # Notice partial=True
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            expense = Expense.objects.get(pk=pk, user=request.user)
+            expense.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Expense.DoesNotExist:
+            return Response({"message": "Expense not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        
+
+
+    
+
+
 
 
 
